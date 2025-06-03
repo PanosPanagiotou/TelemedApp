e2690efd63fd1759c85dabba82c9e8a056604578
Panagiotis Panagiotou <p_panagiotou@hotmail.co.uk>
2025-06-03 17:17:52 +0300

    adding appointment

---
+++ TelemedApp.Application/Interfaces/IAppointmentService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TelemedApp.Domain.Entities;
+
+namespace TelemedApp.Application.Interfaces
+{
+    public interface IAppointmentService
+    {
+        Task<bool> IsDoctorAvailable(Guid doctorId, DateTime start, DateTime end);
+        Task<bool> IsPatientAvailable(Guid patientId, DateTime start, DateTime end);
+        Task<Appointment> CreateAppointmentAsync(Appointment appointment);
+        Task<IEnumerable<Appointment>> GetAppointmentsAsync(Guid? doctorId = null, Guid? patientId = null);
+        Task<bool> CancelAppointmentAsync(Guid appointmentId);
+        // Add update, delete as needed
+    }
+}
