cf9673ec93266503805138e2ace2808aa2ad85c0
Panagiotis Panagiotou <p_panagiotou@hotmail.co.uk>
2025-06-03 16:57:00 +0300

    Adding the UI

---
+++ TelemedApp.API/Controllers/PatientsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TelemedApp.Application.DTOs;
+using TelemedApp.Application.Interfaces;
+
+namespace TelemedApp.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PatientsController(IPatientService patientService) : ControllerBase
+    {
+        private readonly IPatientService _patientService = patientService;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var patients = await _patientService.GetAllPatientsAsync();
+            return Ok(patients);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(PatientDto patient)
+        {
+            await _patientService.CreatePatientAsync(patient);
+            return Ok();
+        }
+
+        // Add GetById, Update, Delete as needed
+    }
+}
